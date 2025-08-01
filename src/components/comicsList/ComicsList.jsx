aeb74867e8aedf921882bc94864d3d9f80c27618
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-08-01 23:24:17 +0300

    add react-transition-group for comicsList

--- src/components/comicsList/ComicsList.jsx
+++ src/components/comicsList/ComicsList.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { Link } from "react-router-dom";
 import useMarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
@@ -39,7 +40,8 @@ const ComicsList = () => {
   function renderItems(arr) {
     const items = arr.map((item) => {
       return (
-        <li className="comics__item" key={item.id}>
+        <CSSTransition key={item.id} timeout={500} classNames={"comics__item"}>
+          <li className="comics__item" >
           <Link to={`/comics/${item.id}`}>
             <img
               src={item.thumbnail}
@@ -50,10 +52,15 @@ const ComicsList = () => {
             <div className="comics__item-price">{item.price}</div>
           </Link>
         </li>
+        </CSSTransition>
       );
     });
 
-    return <ul className="comics__grid">{items}</ul>;
+    return (
+      <ul className="comics__grid">
+        <TransitionGroup component={null}>{items}</TransitionGroup>
+      </ul>
+    );
   }
 
   const items = renderItems(comicsList);
